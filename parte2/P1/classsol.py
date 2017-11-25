d3aad24936e9404bb044879de9f5734fd45582eb
Goncalo Marques <goncalo.cool@gmail.com>
2017-11-25 16:57:41 +0000

    First initiative

--- parte2/P1/classsol.py
+++ parte2/P1/classsol.py
@@ -11,15 +11,28 @@ def features(X):
     F = np.zeros((len(X),5))
     for x in range(0,len(X)):
         F[x,0] = len(X[x])
-        F[x,1] = 
-        F[x,2] = 
-        F[x,3] = 
-        F[x,4] = 
+        F[x,1] = numberOfVowels(X[x])
+        F[x,2] = numberOfConsonants(X[x])
+        F[x,3] = #Percentagem de vogais
+        F[x,4] = #Numero/Percentagem de caracteres esquisitos? (ç Ù)...
 
-    return F     
+    return F
+
+def numberOfVowels(X):
+    count = 0
+    if (letter in X == "a" or "e" or "i" or "o" or "u"):
+        count++
+    return count
+
+def numberOfConsonants(X):
+    count = 0
+    if (not (letter in X == "a" or "e" or "i" or "o" or "u")):
+        count++
+    return count
 
 def mytraining(f,Y):
-    
+    clf = #Implementar
+    clf = clf.fit(f, Y)
    
     return clf
     
@@ -32,3 +45,4 @@ def myprediction(f, clf):
 
     return Ypred
 
+print(numberOfVowels("Lolitos"))
