275bd356d0b1279aec96e64015be6df6ce8a4d6b
Goncalo Marques <goncalo.cool@gmail.com>
2017-12-06 18:29:47 +0000

    Exercício 3 com copy paste generoso

--- parte2/P2/regsol.py
+++ parte2/P2/regsol.py
@@ -5,25 +5,10 @@ from sklearn.model_selection import cross_val_score
 import timeit
 
 def mytraining(X,Y):
-    #Nao funciona
-    """
-    reg = tree.DecisionTreeRegressor(criterion='mse', splitter='best', max_depth=None, \
-                                    min_samples_split=2, min_samples_leaf=1, min_weight_fraction_leaf=0.0, \
-                                    max_features=None, random_state=None, max_leaf_nodes=None, \
-                                    min_impurity_decrease=0.0, min_impurity_split=None, presort=False)
-    """
-    #2º exemplo
-    reg = KernelRidge(alpha=0.01, kernel='polynomial', gamma=1.0, degree=4, coef0=1, kernel_params=None)
+    reg = KernelRidge(alpha=0.01, kernel='polynomial', gamma=1, degree=4, coef0=1, kernel_params=None)
     reg.fit(X,Y)
     return reg
-    
-def mytrainingaux(X,Y,par):
-    
-    #reg.fit(X,Y)
-    return
 
 def myprediction(X,reg):
-
     Ypred = reg.predict(X)
-
     return Ypred
--- parte2/P3/RLsol.py
+++ parte2/P3/RLsol.py
@@ -7,7 +7,7 @@ Created on Mon Oct 16 20:31:54 2017
 import numpy as np
 
 def Q2pol(Q, eta=5):
-	return 
+    return np.exp(eta*Q)/np.dot(np.exp(eta*Q),np.array([[1,1],[1,1]]))
 	
 class myRL:
 
@@ -15,10 +15,22 @@ class myRL:
         self.nS = nS
         self.nA = nA
         self.gamma = gamma
+        self.alpha = 0.01
+        self.threshold = 1e-2
         self.Q = np.zeros((nS,nA))
         
     def traces2Q(self, trace):
-        # implementar esta funcao
+        nQ = np.zeros((self.nS,self.nA))
+        ii = 0
+        while True:            
+            for tt in trace:
+                #[x, a, y, r]
+                nQ[int(tt[0]),int(tt[1])] = nQ[int(tt[0]),int(tt[1])] + self.alpha * (tt[3] + self.gamma * max(nQ[int(tt[2]),:]) - nQ[int(tt[0]),int(tt[1])])
+            err = np.linalg.norm(self.Q-nQ)
+            self.Q = np.copy(nQ)
+            if err < self.threshold:
+                break 
+            ii += 1
         
         return self.Q
 
