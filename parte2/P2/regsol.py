05a1b911843425cbd2241ddf707182391487de85
Gonçalo Marques <goncalo.alex.marx@gmail.com>
2017-12-07 15:17:11 +0000

    P3 OK

--- parte2/P2/regsol.py
+++ parte2/P2/regsol.py
@@ -1,12 +1,12 @@
 import numpy as np
 from sklearn import datasets, tree, linear_model, svm
 from sklearn.kernel_ridge import KernelRidge
+from sklearn.ensemble import RandomForestRegressor
 from sklearn.model_selection import cross_val_score
 import timeit
 
 def mytraining(X,Y):
-    reg = svm.SVR(C=2, epsilon=0.06, gamma=0.8)
-    #Funciona nos dois exemplos
+    reg = svm.SVR(C=1000, gamma=0.1)
     #reg = KernelRidge(alpha=0.001, kernel='rbf', gamma=0.1)
     reg.fit(X,Y)
     return reg
--- parte2/P2/testregsol.py
+++ parte2/P2/testregsol.py
@@ -10,8 +10,7 @@ from matplotlib.colors import ListedColormap
 import regsol
 
 tres = [.3, 800]    
-#for ii,test in enumerate(['regress.npy', 'regress2.npy']):
-for ii,test in enumerate(['regress.npy']):
+for ii,test in enumerate(['regress.npy', 'regress2.npy']):
     print("Testing " + test)
     
     X,Y,Xp,Yp = np.load(test)
