2d097001f955a76998c01b8134629558044b9c21
Goncalo Marques <goncalo.cool@gmail.com>
2017-12-09 21:55:35 +0000

    Improvement P3

--- parte2/P2/regsol.py
+++ parte2/P2/regsol.py
@@ -6,18 +6,16 @@ from sklearn.model_selection import cross_val_score
 import timeit
 
 def mytraining(X,Y):
-    #Accuracy: -0.17; -62.50
-    #reg = SVR(C=1000, gamma=0.1)
+    #Accuracy: -0.17; -582.91
+    reg = SVR(C=1000, gamma=0.1)
 
-    #Accuracy: -0.12; -37.23
-    reg = KernelRidge(alpha=0.001, kernel='rbf', gamma=0.1)
+    #Accuracy: -0.10; -547.94
+    #reg = KernelRidge(alpha=0.001, kernel='rbf', gamma=0.1)
     reg.fit(X,Y)
 
-    """
-    cv = cross_val_score(reg, X, Y, scoring='neg_mean_squared_error', cv=13)
+    cv = cross_val_score(reg, X, Y, scoring='neg_mean_squared_error', cv=5)
     print("cv: ", cv)
     print("Accuracy: %0.2f (+/- %0.2f)" % (cv.mean(), cv.std() * 2))
-    """
 
     return reg
 
